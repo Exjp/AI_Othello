40554bb0ad8cf63d7f5935c36676934d26f9b1b4
Exjp <exilfaker@gmail.com>
2019-11-29 17:09:04 +0100

    alphabeta minmax fonctionnel

--- myPlayer.py
+++ myPlayer.py
@@ -21,12 +21,6 @@ class myPlayer(PlayerInterface):
         else:
             return -1
 
-    def getresult(self,color):
-        if self._mycolor == color:
-            return 1
-        else:
-            return -1
-    
     def maxValue(self, alpha, beta,color,depth):
         if self._board.is_game_over() or depth == 0:
             res = self._board.heuristique()
@@ -60,8 +54,9 @@ class myPlayer(PlayerInterface):
         
         return beta
 
-    def getPlayerMove(self):
-        
+#    def sendMove(self,x,y):
+#        return()
+    def alphabeta(self,color):
         better = -1
         tmp = []
 
@@ -69,17 +64,34 @@ class myPlayer(PlayerInterface):
             self._board.push(move)
             res = self.minValue(-100, 100,self._mycolor,3)
             self._board.pop()
-            (c,x,y) = move
             if res > 0:
-                return (x,y)
+                return move
             elif res <= 0:
                 if len(tmp) == 0:
-                    tmp = (x,y)
+                    tmp = move
             elif res == 0 and better -1:
                 better = 0
-                tmp = (x,y)
+                tmp = move
 
         return  tmp
+        
+
+    def getPlayerMove(self):
+        if self._board.is_game_over():
+            print("Referee told me to play but the game is over!")
+            return (-1,-1);
+        if self._mycolor == self._board._BLACK:
+            move = self.alphabeta(self._mycolor)
+        elif self._mycolor == self._board._WHITE:
+            moves = [m for m in self._board.legal_moves()]
+            move = moves[randint(0,len(moves)-1)]
+        self._board.push(move)
+        print("I am playing ", move)
+        (c,x,y) = move
+        assert(c==self._mycolor)
+        print("My current board :")
+        print(self._board)
+        return (x,y) 
 
     
     def playOpponentMove(self, x,y):
